660a43905164ae5d0921d7bd3d7ad5bf91ac7651
pradeepgangwar <pradeepgangwar39@gmail.com>
2018-12-22 17:06:45 +0530

    deploy to heroku

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  clientId: 'Fl-hdc6xdYIkok9ynbcL6zoUZPAIdOZN',
+  domain: 'hasura-react-apollo-todo.auth0.com',
+  callbackUrl: 'http://localhost:3000/callback',
+  apiUrl: 'YOUR_API_IDENTIFIER'
+}
